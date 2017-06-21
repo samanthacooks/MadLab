1186a4033827679682442a235444530fc4868937
Hector Feurtado <email": null>
2017-06-21 16:48:24 -0400

    add routes controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :experiments do
+    resources :proposals
+    resources :comments
+    resources :observations
+  end
 end
