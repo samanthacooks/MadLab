9c278950653e3199ac863d19867700e197a20167
Hector Feurtado <email": null>
2017-06-21 16:55:14 -0400

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :experiments do
-    resources :proposals
+    resources :proposals,except: [:index]
     resources :comments
     resources :observations
   end
+  root "welcome#index"
 end
