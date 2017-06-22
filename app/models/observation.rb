296aae868192b5b5bd46e481e42df38f4fb94aca
Hector Feurtado <email": null>
2017-06-22 12:05:47 -0400

    add validations

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,4 +1,6 @@
 class Observation < ApplicationRecord
   belongs_to :observable, polymorphic: true
   has_many :comments, as: :commentable
+
+  validates :body, :observable, presence:true
 end
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,5 +1,6 @@
 class Procedure < ApplicationRecord
   belongs_to :experiment
   has_many :observations, as: :observable
+  validates :steps, :experiment_id, presence:true
 
 end
