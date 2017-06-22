7c12d0327d63a928b18db7344ac73cf00e34cd32
Samantha T Cooks <samtcooks@gmail.com>
2017-06-22 09:41:51 -0400

    Finish user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :proposals do
-    resources :experiments
+  resources :users
+
+  resources :proposals
+
+  resources :experiments do
     resources :comments
     resources :observations
   end
