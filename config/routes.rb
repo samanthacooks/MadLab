23ca3bad8c39bb230d45c6ab421fa96786337aa2
Hector Feurtado <email": null>
2017-06-23 10:04:47 -0400

    create new experiment on proposal

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :proposals
-  resources :experiments do
+
+  resources :proposals do
+    resources :experiments,only:[:new,:create]
+  end
+
+  resources :experiments,except:[:new,:create] do
     resources :procedures
     resources :comments,except:[:create,:destroy]
     resources :observations,except:[:destroy]
   end
+
     post '/experiments/:experiment_id/comments' => 'comments#create', as: 'experiments_comment_post'
     delete '/experiments/:experiment_id/comments/:id' => 'comments#destroy', as: 'experiments_comment_post_destroy'
     delete '/experiments/:experiment_id/observations/:id' => 'observations#destroy', as: 'destroy_exp_observation_destroy'
