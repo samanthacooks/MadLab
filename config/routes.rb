d207ca3267b3bd5f7c35c7107347572dcb50ac65
Hector Feurtado <email": null>
2017-06-22 22:40:09 -0400

    add prefix to observation destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   resources :experiments do
     resources :procedures
     resources :comments,except:[:create,:destroy]
-    resources :observations
+    resources :observations,except:[:destroy]
   end
     post '/experiments/:experiment_id/comments' => 'comments#create', as: 'experiments_comment_post'
     delete '/experiments/:experiment_id/comments/:id' => 'comments#destroy', as: 'experiments_comment_post_destroy'
+    delete '/experiments/:experiment_id/observations/:id' => 'observations#destroy', as: 'destroy_exp_observation_destroy'
     get '/about' => "welcome#about"
     get '/help' => "welcome#help"
     root "welcome#index"
